96d38f40ded2324ba3f55756545227714be9387f
William <tianshiwxd@126.com>
2018-09-27 15:19:51 +0800

    Update Goods.php

--- models/Goods.php
+++ models/Goods.php
@@ -12,15 +12,18 @@ use yii\web\UploadedFile;
  *
  * @property int $id 自增id
  * @property string $goods_number 零件号
- * @property string $description 描述
+ * @property string $description 中文描述
+ * @property string $description_en 英文描述
  * @property string $original_company 原厂家
  * @property string $original_company_remark 原厂家备注
  * @property string $unit 单位
  * @property string $technique_remark 技术备注
  * @property string $img_id 图纸
  * @property int $is_process 是否加工
+ * @property int $is_special 是否特制
+ * @property int $is_nameplate 是否铭牌
+ * @property int $nameplate_img_id 铭牌照片
  * @property int $is_deleted 是否删除：0未删除 1已删除
- * @property string $offer_date 报价时间
  * @property string $updated_at 更新时间
  * @property string $created_at 创建时间
  */
@@ -31,12 +34,28 @@ class Goods extends ActiveRecord
 
     const IS_PROCESS_NO    = '0';
     const IS_PROCESS_YES   = '1';
+    
+    const IS_SPECIAL_NO    = '0';
+    const IS_SPECIAL_YES   = '1';
+    
+    const IS_NAMEPLATE_NO    = '0';
+    const IS_NAMEPLATE_YES   = '1';
 
     public static $process = [
         self::IS_PROCESS_NO  => '否',
         self::IS_PROCESS_YES => '是',
     ];
 
+    public static $special = [
+        self::IS_SPECIAL_NO  => '否',
+        self::IS_SPECIAL_YES => '是',
+    ];
+    
+    public static $nameplate = [
+        self::IS_NAMEPLATE_NO  => '否',
+        self::IS_NAMEPLATE_YES => '是',
+    ];
+    
     public $img_url = '';
 
     public function behaviors()
@@ -69,9 +88,9 @@ class Goods extends ActiveRecord
     public function rules()
     {
         return [
-            [['is_process', 'is_deleted'], 'integer'],
+            [['is_process', 'is_deleted', 'is_special', 'is_nameplate'], 'integer'],
             [['offer_date', 'updated_at', 'created_at', 'img_url'], 'safe'],
-            [['goods_number', 'original_company', 'original_company_remark', 'unit', 'technique_remark', 'img_id'], 'string', 'max' => 255],
+            [['goods_number', 'original_company', 'original_company_remark', 'unit', 'technique_remark', 'img_id', 'nameplate_img_id'], 'string', 'max' => 255],
             [['description'], 'string', 'max' => 512],
             [
                 ['goods_number','description'],
@@ -96,6 +115,9 @@ class Goods extends ActiveRecord
             'technique_remark'        => '技术备注',
             'img_id'                  => '图纸',
             'is_process'              => '是否加工',
+            'is_special'              => '是否特制',
+            'is_nameplate'            => '是否铭牌',
+            'nameplate_img_id'        => '铭牌照片',
             'is_deleted'              => '是否删除：0未删除 1已删除',
             'updated_at'              => '更新时间',
             'created_at'              => '创建时间',
@@ -108,6 +130,9 @@ class Goods extends ActiveRecord
         if ($this->img_id) {
             $this->img_url = sprintf('%s/%s', Yii::$app->params['img_url_prefix'], $this->img_id);
         }
+        if ($this->nameplate_img_id) {
+            $this->nameplate_img_url = sprintf('%s/%s', Yii::$app->params['img_url_prefix'], $this->nameplate_img_id);
+        }
     }
 
     public function beforeSave($insert)
@@ -118,7 +143,6 @@ class Goods extends ActiveRecord
             return false;
         }
         $img = UploadedFile::getInstance($this, 'img_id');
-
         if ($img) {
             //用云
             $this->img_id = Util::upload($img->tempName);
@@ -131,6 +155,15 @@ class Goods extends ActiveRecord
             $this->img_id = $this->getOldAttribute('img_id') ? $this->getOldAttribute('img_id') : '';
         }
 
+        $nameplate_img = UploadedFile::getInstance($this, 'nameplate_img_id');
+        if ($nameplate_img) {
+            //用云
+            $this->nameplate_img_id = Util::upload($nameplate_img->tempName);
+        }
+        else {
+            $this->nameplate_img_id = $this->getOldAttribute('nameplate_img_id') ? $this->getOldAttribute('nameplate_img_id') : '';
+        }
+        
         // 删除零件时，如果存在则不能删除
         if ($this->is_deleted == static::IS_DELETED_YES ) { // 删除操作
 
