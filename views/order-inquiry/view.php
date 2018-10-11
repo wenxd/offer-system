d9d2a4aab019824cc0a4ffe405b677e86db1a480
William <tianshiwxd@126.com>
2018-10-11 09:35:56 +0800

    Update view.php

--- views/order-inquiry/view.php
+++ views/order-inquiry/view.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <th>订单号</th>
                 <th>零件号</th>
                 <th>是否询价</th>
-                <th>操作</th>
+                <th width="300px">操作</th>
             </tr>
             </thead>
             <tbody>
@@ -32,7 +32,9 @@ $this->params['breadcrumbs'][] = $this->title;
                     <td><?=$item->goods->goods_number?></td>
                     <td><?=$item::$Inquiry[$item->is_inquiry]?></td>
                     <td>
-                        <a class="btn btn-success btn-xs btn-flat" href="?r=order-inquiry/confirm&id=<?=$item->id?>" data-pjax="0"><i class="fa fa-hand-pointer-o"></i> 确认询价完成</a>
+                        <?php if (!$item->is_inquiry):?>
+                            <a class="btn btn-success btn-xs btn-flat confirm" data-id="<?=$item->id?>" href="javascript:void(0);" data-pjax="0"><i class="fa fa-hand-pointer-o"></i> 确认询价完成</a>
+                        <?php endif;?>
                         <a class="btn btn-primary btn-xs btn-flat" href="?r=inquiry/create" data-pjax="0"><i class="fa fa-plus"></i> 添加询价记录</a>
                     </td>
                 </tr>
@@ -45,5 +47,25 @@ $this->params['breadcrumbs'][] = $this->title;
 <?=Html::jsFile('@web/js/jquery-3.2.1.min.js')?>
 <script type="text/javascript" src="./js/layer.js"></script>
 <script type="text/javascript">
-
+    $(document).ready(function () {
+        $('.confirm').click(function (e) {
+            var id = $(this).data('id');
+            $.ajax({
+                type:"get",
+                url:'?r=order-inquiry/confirm',
+                data:{id:id},
+                dataType:'JSON',
+                success:function(res){
+                    if (res && res.code == 200){
+                        layer.msg(res.msg, {time:1000}, function () {
+                            location.reload();
+                        });
+                    } else {
+                        layer.msg(res.msg, {time:2000});
+                        return false;
+                    }
+                }
+            });
+        });
+    });
 </script>
