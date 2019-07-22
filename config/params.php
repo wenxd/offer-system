b96115613ee850845b8420ce9fb1d52d4bc520a9
wenxd <wenxiangduo@sunlands.com>
2019-07-22 16:42:19 +0800

    x

--- config/params.php
+++ config/params.php
@@ -6,6 +6,6 @@ return [
         'defaultUserStatus' => 0, // 0 = inactive, 10 = active
         'userTable' => 'admin'
     ],
-    //'img_url_prefix' => 'https://quote-1253507986.cos.ap-beijing.myqcloud.com',
+    'img_url_prefix' => 'https://quote-1253507986.cos.ap-beijing.myqcloud.com',
     //'img_url_prefix' => 'http://offer.com',
 ];
