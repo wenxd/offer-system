315610e6fea81d78958767610e1a14ee1fc6e9ba
wenxd <wenxiangduo@sunlands.com>
2019-09-02 20:52:57 +0800

    x

--- models/QuoteGoodsSearch.php
+++ models/QuoteGoodsSearch.php
@@ -76,38 +76,38 @@ class QuoteGoodsSearch extends QuoteGoods
 
         // grid filtering conditions
         $query->andFilterWhere([
-            'id' => $this->id,
-            'order_id' => $this->order_id,
-            'order_final_id' => $this->order_final_id,
-            'goods_id' => $this->goods_id,
-            'type' => $this->type,
-            'relevance_id' => $this->relevance_id,
-            'number' => $this->number,
-            'is_quote' => $this->is_quote,
-            'is_deleted' => $this->is_deleted,
-            'updated_at' => $this->updated_at,
-            'tax_rate' => $this->tax_rate,
-            'price' => $this->price,
-            'tax_price' => $this->tax_price,
-            'all_price' => $this->all_price,
-            'all_tax_price' => $this->all_tax_price,
-            'quote_price' => $this->quote_price,
-            'quote_tax_price' => $this->quote_tax_price,
-            'quote_all_price' => $this->quote_all_price,
-            'quote_all_tax_price' => $this->quote_all_tax_price,
-            'delivery_time' => $this->delivery_time,
+            'quote_goods.id' => $this->id,
+            'quote_goods.order_id' => $this->order_id,
+            'quote_goods.order_final_id' => $this->order_final_id,
+            'quote_goods.goods_id' => $this->goods_id,
+            'quote_goods.type' => $this->type,
+            'quote_goods.relevance_id' => $this->relevance_id,
+            'quote_goods.number' => $this->number,
+            'quote_goods.is_quote' => $this->is_quote,
+            'quote_goods.is_deleted' => $this->is_deleted,
+            'quote_goods.updated_at' => $this->updated_at,
+            'quote_goods.tax_rate' => $this->tax_rate,
+            'quote_goods.price' => $this->price,
+            'quote_goods.tax_price' => $this->tax_price,
+            'quote_goods.all_price' => $this->all_price,
+            'quote_goods.all_tax_price' => $this->all_tax_price,
+            'quote_goods.quote_price' => $this->quote_price,
+            'quote_goods.quote_tax_price' => $this->quote_tax_price,
+            'quote_goods.quote_all_price' => $this->quote_all_price,
+            'quote_goods.quote_all_tax_price' => $this->quote_all_tax_price,
+            'quote_goods.delivery_time' => $this->delivery_time,
         ]);
 
-        $query->andFilterWhere(['like', 'order_final_sn', $this->order_final_sn])
-            ->andFilterWhere(['like', 'order_quote_id', $this->order_quote_id])
-            ->andFilterWhere(['like', 'order_quote_sn', $this->order_quote_sn])
-            ->andFilterWhere(['like', 'serial', $this->serial]);
+        $query->andFilterWhere(['like', 'quote_goods.order_final_sn', $this->order_final_sn])
+              ->andFilterWhere(['like', 'quote_goods.order_quote_id', $this->order_quote_id])
+              ->andFilterWhere(['like', 'quote_goods.order_quote_sn', $this->order_quote_sn])
+              ->andFilterWhere(['like', 'quote_goods.serial', $this->serial]);
 
         if ($this->created_at && strpos($this->created_at, ' - ')) {
             list($created_at_start, $created_at_end) = explode(' - ', $this->created_at);
             $created_at_start .= ' 00:00:00';
             $created_at_end   .= ' 23::59:59';
-            $query->andFilterWhere(['between', 'order_final.created_at', $created_at_start, $created_at_end]);
+            $query->andFilterWhere(['between', 'quote_goods.created_at', $created_at_start, $created_at_end]);
         }
 
         return $dataProvider;
