37b5d36c73f2ac50b60f9fa68ecff1a95d9b8f4c
wenxd <wenxiangduo@sunlands.com>
2019-09-20 20:29:51 +0800

    x

--- views/order-quote/view.php
+++ views/order-quote/view.php
@@ -28,17 +28,14 @@ foreach ($adminList as $key => $admin) {
             <thead class="data" data-order_final_id="<?=$_GET['id']?>">
             <tr>
                 <th>序号</th>
+                <th>零件号</th>
                 <th>厂家号</th>
                 <th>中文描述</th>
                 <th>英文描述</th>
                 <th>原厂家</th>
                 <th>原厂家备注</th>
+                <th>订单需求数量</th>
                 <th>单位</th>
-                <th>技术备注</th>
-                <th>加工</th>
-                <th>特制</th>
-                <th>铭牌</th>
-                <th>图片</th>
                 <th>供应商</th>
                 <th>税率</th>
                 <th>货期(周)</th>
@@ -51,11 +48,7 @@ foreach ($adminList as $key => $admin) {
                 <th>报价含税单价</th>
                 <th>报价未税总价</th>
                 <th>报价含税总价</th>
-                <th>询价状态</th>
-                <th>是否有报价单</th>
-                <th>报价单号</th>
                 <th>库存数量</th>
-                <th>订单需求数量</th>
             </tr>
             </thead>
             <tbody>
@@ -63,16 +56,13 @@ foreach ($adminList as $key => $admin) {
                 <tr class="order_final_list">
                     <td class="serial"><?=$item->serial?></td>
                     <td><?=Html::a($item->goods->goods_number, Url::to(['goods/search-result', 'good_number' => $item->goods->goods_number]))?></td>
+                    <td><?=Html::a($item->goods->goods_number_b, Url::to(['goods/search-result', 'good_number' => $item->goods->goods_number]))?></td>
                     <td><?=$item->goods->description?></td>
                     <td><?=$item->goods->description_en?></td>
                     <td><?=$item->goods->original_company?></td>
                     <td><?=$item->goods->original_company_remark?></td>
+                    <td class="afterNumber"><?=$item->number?></td>
                     <td><?=$item->goods->unit?></td>
-                    <td><?=$item->goods->technique_remark?></td>
-                    <td><?=Goods::$process[$item->goods->is_process]?></td>
-                    <td><?=Goods::$special[$item->goods->is_special]?></td>
-                    <td><?=Goods::$nameplate[$item->goods->is_nameplate]?></td>
-                    <td><?=Html::img($item->goods->img_url, ['width' => '50px'])?></td>
                     <td><?=$item->type ? $item->stock->supplier->name : $item->inquiry->supplier->name?></td>
                     <td class="ratio"><?=$item->tax_rate?></td>
                     <td class="delivery_time"><?=$item->delivery_time?></td>
@@ -96,29 +86,27 @@ foreach ($adminList as $key => $admin) {
                             }
                         }
                     ?>
-                    <td><?=$is_inquiry ?  '已询价' : '未询价'?></td>
-                    <td><?=$item->order_quote_id ? '是' : '否'?></td>
-                    <td><?=$item->order_quote_sn?></td>
                     <td><?=$item->stockNumber ? $item->stockNumber->number : 0?></td>
-                    <td class="afterNumber"><?=$item->number?></td>
                 </tr>
             <?php endforeach;?>
             <tr style="background-color: #acccb9">
-                <td colspan="15" rowspan="2">汇总统计</td>
+                <td colspan="12" rowspan="2">汇总统计</td>
                 <td rowspan="2">成本单</td>
                 <td>最长货期</td>
                 <td>未税总价</td>
                 <td>含税总价</td>
-                <td colspan="2" rowspan="2"></td>
+                <td rowspan="2"></td>
                 <td rowspan="2">报价单</td>
+                <td>报价最长货期</td>
                 <td>报价未税总价</td>
                 <td>报价含税总价</td>
-                <td colspan="5" rowspan="2"></td>
+                <td colspan="2" rowspan="2"></td>
             </tr>
             <tr style="background-color: #acccb9">
                 <td class="mostLongTime"></td>
                 <td class="sta_all_price"></td>
                 <td class="sta_all_tax_price"></td>
+                <td class="quote_mostLongTime"></td>
                 <td class="sta_quote_all_price"></td>
                 <td class="sta_quote_all_tax_price"></td>
             </tr>
@@ -147,8 +135,9 @@ foreach ($adminList as $key => $admin) {
             var sta_all_tax_price       = 0;
             var sta_quote_all_price     = 0;
             var sta_quote_all_tax_price = 0;
+            var quote_mostLongTime      = 0;
             $('.order_final_list').each(function (i, e) {
-                var delivery_time   = parseFloat($(e).find('.quote_delivery_time').text());
+                var delivery_time   = parseFloat($(e).find('.delivery_time').text());
                 if (delivery_time > mostLongTime) {
                     mostLongTime = delivery_time;
                 }
@@ -168,12 +157,18 @@ foreach ($adminList as $key => $admin) {
                 if (quote_all_tax_price) {
                     sta_quote_all_tax_price += parseFloat(quote_all_tax_price);
                 }
+                var quote_delivery_time   = parseFloat($(e).find('.quote_delivery_time').text());
+                if (quote_delivery_time > mostLongTime) {
+                    quote_mostLongTime = quote_delivery_time;
+                }
+
             });
             $('.mostLongTime').text(mostLongTime);
             $('.sta_all_price').text(sta_all_price.toFixed(2));
             $('.sta_all_tax_price').text(sta_all_tax_price.toFixed(2));
             $('.sta_quote_all_price').text(sta_quote_all_price.toFixed(2));
             $('.sta_quote_all_tax_price').text(sta_quote_all_tax_price.toFixed(2));
+            $('.quote_mostLongTime').text(quote_mostLongTime);
         }
     });
 </script>
