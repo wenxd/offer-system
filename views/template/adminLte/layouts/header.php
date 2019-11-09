b11de78e6eed99fba52de3f790e60540e056793d
wenxd <tianshiwxd@126.com>
2019-11-09 12:28:43 +0800

    x

--- views/template/adminLte/layouts/header.php
+++ views/template/adminLte/layouts/header.php
@@ -1,9 +1,17 @@
 <?php
+
+use app\models\AuthAssignment;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use app\models\SystemNotice;
 /* @var $this \yii\web\View */
 /* @var $content string */
 $identity = Yii::$app->user->getIdentity();
+$user_id = $identity->id;
+
+$use_admin = AuthAssignment::find()->where(['item_name' => ['系统管理员', '采购员']])->all();
+$adminIds  = ArrayHelper::getColumn($use_admin, 'user_id');
+
 //统计每次气泡数量
 $notice_number = SystemNotice::find()->where([
         'is_deleted' => SystemNotice::IS_DELETED_NO,
@@ -35,11 +43,13 @@ $notice_number = SystemNotice::find()->where([
                         <i class="glyphicon glyphicon-user"></i> <span><?= $identity->username ?></span>
                     </a>
                 </li>
-                <li>
-                    <a href="?r=system-notice/index">
-                        <i class="fa fa-bell-o font-20"></i> <span class="label label-success ng-binding" style="background-color:#d24637;z-index:99999"><?=$notice_number?></span>
-                    </a>
-                </li>
+                <?php if (in_array($user_id, $adminIds)):?>
+                    <li>
+                        <a href="?r=system-notice/index">
+                            <i class="fa fa-bell-o font-20"></i> <span class="label label-success ng-binding" style="background-color:#d24637;z-index:99999"><?=$notice_number?></span>
+                        </a>
+                    </li>
+                <?php endif;?>
                 <li>
                     <?= Html::a(
                         '<i class="glyphicon glyphicon-log-out"></i> 退出',
