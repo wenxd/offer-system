731b9fcb6143813f522240c7e591f1449d4ceac9
william <wenxiangduo@sunlands.com>
2020-06-21 17:14:02 +0800

    x

--- views/stock-out-log/index.php
+++ views/stock-out-log/index.php
@@ -75,6 +75,7 @@ $userId   = Yii::$app->user->identity->id;
                 [
                     'attribute' => 'goods_number',
                     'format'    => 'raw',
+                    'contentOptions' => ['style'=>'min-width: 100px;'],
                     'label'     => '零件号',
                     'filter'    => Html::activeTextInput($searchModel, 'goods_number',['class'=>'form-control']),
                     'value'     => function ($model, $key, $index, $column) {
